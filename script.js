567bafcb90816016b4f0c7535220a017e58b3c56
Sebastian Brear <ssbrear@ucdavis.edu>
2020-08-04 22:22:53 -0700

    Finalized New York Times Search

---
+++ script.js
@@ -0,0 +1,46 @@
+const API_KEY = "MbREfdD9Y4pqheqDKUJQtMnWKlAhR9ZH";
+var searchTerm = "";
+var queryURL = "";
+var numRecords = 0;
+var articlesHere = $("#articles-here");
+var startYear = 0;
+var endYear = 0;
+
+function updateQuery() {
+    searchTerm = $("#search").val();
+    numRecords = $("#num-records").val();
+    startYear = $("#start-year").val();
+    endYear = $("#end-year").val();
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + startYear + "0101&end_date=" + endYear + "0101&q=" + searchTerm + "&api-key=" + API_KEY;
+}
+
+function clearQuery() {
+    $("#articles-here").empty();
+}
+
+
+$("#submit-button").on("click", function(event) {
+    event.preventDefault()
+
+    updateQuery();
+
+    $.ajax({
+        url: queryURL
+    }).then(function(query) {
+        clearQuery();
+        for (var i = 0; i < numRecords; i++) {
+            var newArticle = query.response.docs[i];
+
+            var newHeader = $("<div>");
+            newHeader.text(newArticle.headline.main);
+            newHeader.css({"fontSize":"24px", "fontWeight":"bold", "marginTop":"8px", "marginBottom":"8px;"});
+
+            articlesHere.append(newHeader);
+        }
+    });
+});
+
+$("#clear-button").on("click", function(event) {
+    event.preventDefault();
+    clearQuery();
+})
